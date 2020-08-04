2cf6b7f935a63e6fa45b46d0e82fe580c7eb7779
Elizabeth5 <escheidt5@gmail.com>
2020-08-04 16:24:59 -0400

    Updates

---
+++ precache-manifest.8e6f1355bad9d2405ccb582f2e1e85da.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cf95e8a2e27a29ea00abca8e4f3722",
+    "url": "/react-sports-game-assessment/index.html"
+  },
+  {
+    "revision": "e04300e827749968e80e",
+    "url": "/react-sports-game-assessment/static/css/main.c884662b.chunk.css"
+  },
+  {
+    "revision": "83ec443dbaee11d6f020",
+    "url": "/react-sports-game-assessment/static/js/2.fbb68554.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sports-game-assessment/static/js/2.fbb68554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04300e827749968e80e",
+    "url": "/react-sports-game-assessment/static/js/main.a568e16f.chunk.js"
+  },
+  {
+    "revision": "869bfc85debc3fcd1f5b",
+    "url": "/react-sports-game-assessment/static/js/runtime-main.4bd516d6.js"
+  },
+  {
+    "revision": "59f365b90752f50ac04f2b8b243de5e8",
+    "url": "/react-sports-game-assessment/static/media/background.59f365b9.jpg"
+  },
+  {
+    "revision": "a26d4543e0224178273e0ed70894b07c",
+    "url": "/react-sports-game-assessment/static/media/logo1.a26d4543.png"
+  },
+  {
+    "revision": "2594219ce5cd3f6f16f8af8e5109be59",
+    "url": "/react-sports-game-assessment/static/media/logo2.2594219c.jfif"
+  },
+  {
+    "revision": "3776dfaf2df3ed64a92f90b63fa2c1b8",
+    "url": "/react-sports-game-assessment/static/media/logo3.3776dfaf.jfif"
+  },
+  {
+    "revision": "6c66ad1afc430a31a12c42af92c89ccf",
+    "url": "/react-sports-game-assessment/static/media/logo4.6c66ad1a.png"
+  },
+  {
+    "revision": "ec73a758b80561615126a80f98880234",
+    "url": "/react-sports-game-assessment/static/media/miss.ec73a758.wav"
+  },
+  {
+    "revision": "1c5e2a77b58e97d13af72560d372d1e9",
+    "url": "/react-sports-game-assessment/static/media/score.1c5e2a77.wav"
+  }
+]);
